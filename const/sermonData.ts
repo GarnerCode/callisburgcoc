67f1fcaa2a65deb95cc96e93d0e1e6eff103698e
Tyler Garner <tyler@garnercode.io>
2023-05-23 10:51:47 -0500

    Updated Sermon

--- const/sermonData.ts
+++ const/sermonData.ts
@@ -1,5 +1,5 @@
 export const sermonData = {
-    videoUrl: 'https://www.youtube.com/embed/3laSoh6nrK4',
-    scripture: 'A wife of noble character who can find? She is worth far more than rubies. Her husband has full confidence in her and lacks nothing of value. She brings him good, not harm, all the days of her life.',
-    verse: '-Proverbs 31'
+    videoUrl: 'https://www.youtube.com/embed/KVL3DEgzRUg',
+    scripture: 'For the grace of God has appeared that offers salvation to all people. It teaches us to say “No” to ungodliness and worldly passions, and to live self-controlled, upright and godly lives in this present age, while we wait for the blessed hope—the appearing of the glory of our great God and Savior, Jesus Christ, who gave himself for us to redeem us from all wickedness and to purify for himself a people that are his very own, eager to do what is good.',
+    verse: '-Titus 2:11-14'
 }
